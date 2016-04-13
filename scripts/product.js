841721f67470ebdf41d038099493bfc03b3c8bb2
fancyboynet <fancyboynet@gmail.com>
2016-04-13 11:52:42 +0800

    修复scripts/product.js 路径bug

--- scripts/product.js
+++ scripts/product.js
@@ -12,7 +12,7 @@ if (!shell.which('fis3')) {
 }
 
 shell.rm('-rf', configBuild.output + configBuild.static_root + '/*');
-shell.rm('-rf', configBuild.output + '/' + configBuild.tpl_root+ '/*');
+shell.rm('-rf', configBuild.output + configBuild.tpl_root+ '/*');
 shell.exec('fis3 release prod -cd ' + configBuild.output);
 
 // shell.cp('-rf', configBuild.output + configBuild.static_root + '/', '../../../static/' + configBuild.static_root + '/');
