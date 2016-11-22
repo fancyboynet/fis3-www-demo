7fa4d38fe11190302cd1e21a0b7022bbdf26bf07
Fancy <fancyboynet@gmail.com>
2016-11-22 09:30:05 +0800

    字体文件增加url前缀配置

--- fis-conf.js
+++ fis-conf.js
@@ -1,10 +1,10 @@
-"use strict";
-let configBuild = require('./config/build.json');
+"use strict"
+let configBuild = require('./config/build.json')
 
-let staticRoot = configBuild.static_root; //实际静态资源根目录
-let tplRoot = configBuild.tpl_root; //模版根目录
+let staticRoot = configBuild.static_root //实际静态资源根目录
+let tplRoot = configBuild.tpl_root //模版根目录
 
-fis.hook('commonjs');
+fis.hook('commonjs')
 
 
 //排除不需要产出的目录
@@ -15,13 +15,13 @@ fis.set('project.ignore', fis.get('project.ignore').concat([
     'component.json',
     'package.json',
     'test/**'
-]));
+]))
 
 // 所有的文件产出到 {staticRoot} 目录下
 fis.match('*', {
     release: staticRoot + '$0',
     useHash : false
-});
+})
 
 //排除不需要产出的文件
 fis
@@ -30,40 +30,40 @@ fis
     })
     .match('.gitignore', {
         release: false
-    });
+    })
 
 
 //npm 组件
 fis.match('/{node_modules,components,widget}/**.js', {
     isMod: true,
     useSameNameRequire: true
-});
+})
 
 fis.match('/page/**', {
     isMod: true,
     useSameNameRequire: true
-});
+})
 
 fis.match('/static/**', {
     isMod: false,
     useSameNameRequire: false
-});
+})
 
 // 本地模拟数据产出到根test目录下，否则无法模拟动态数据
 fis.match('/mock/**', {
     release: '$0'
-});
+})
 
 // fis模拟配置文件
 fis.match('/mock/server.conf', {
     release: '/config/server.conf'
-});
+})
 
 // 所有模板放到 {tplRoot} 目录下
 fis.match('*.html', {
     release: tplRoot + '$0',
     parser: fis.plugin('html-uri')
-});
+})
 
 // 前端模板
 fis.match('**.tpl', {
@@ -71,32 +71,32 @@ fis.match('**.tpl', {
     parser: fis.plugin('utc'), // invoke `fis-parser-utc`,
     isJsLike: true,
     isMod:false //避免被当作组件包装
-});
+})
 
 fis.match('**.css', {
     parser: fis.plugin('css-url-hash'),
     postprocessor: fis.plugin('autoprefixer', {
         "browsers": ["last 2 versions", "ie 8"]
     })
-});
+})
 
 fis.match('node_modules/**.css', {
     parser: null,
     postprocessor: null
-});
+})
 
 //数据接口增加实际前缀
 fis.match('/widget/api/config.js', {
     parser: function (content, file, opt) {
         // 只对 html 类文件进行处理
         if (!file.isJsLike){
-            return content;
+            return content
         }
         return content.replace(/\{static_url_prefix\}/g, function(all, value) {
-            return configBuild.static_url_prefix ;
-        });
+            return configBuild.static_url_prefix 
+        })
     }
-});
+})
 
 // 添加css和image加载支持
 fis.match('*.js', {
@@ -106,14 +106,14 @@ fis.match('*.js', {
             useEmbedWhenSizeLessThan: 10 * 1024 // 小于10k用base64
         })
     ]
-});
+})
 
 //babel
 fis.match('/{page,widget}/**.js', {
     parser: fis.plugin('babel-5.x', {
         stage: 3
     })
-});
+})
 
 
 fis.match('::packager', {
@@ -123,14 +123,14 @@ fis.match('::packager', {
         resourceType: 'mod',
         useInlineMap: true // 资源映射表内嵌
     })
-});
+})
 
 
 // 禁用components
-fis.unhook('components');
+fis.unhook('components')
 fis.hook('node_modules', {
     useDev: false
-});
+})
 
 //生产环境
 
@@ -154,7 +154,10 @@ fis.media('prod')
     })
     .match('*.{png,gif,jpg,jpeg}', { //图片引用增加url前缀
         url : configBuild.static_url_prefix  + staticRoot + '$0'
-    });
+    })
+    .match('*.{eot,ttf,woff,svg}', { //字体引用增加url前缀
+        url : configBuild.static_url_prefix  + staticRoot + '$0'
+    })
 
 // pack
 fis.media('prod')
@@ -170,5 +173,5 @@ fis.media('prod')
             layout: 'matrix',
             margin: '15'
         })
-    });
+    })
 
